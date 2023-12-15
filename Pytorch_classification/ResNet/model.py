b0a3965d8408120c0533c3833f7a0deb7a3cccaa
Sirwenhao <1547102356@qq.com>
2023-12-15 23:23:58 +0800

    23/12/15

--- Pytorch_classification/ResNet/model.py
+++ Pytorch_classification/ResNet/model.py
@@ -34,4 +34,65 @@ class BasicBlock(nn.Module):
 
         return out
 
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, groups=1, width_per_group=64):
+        super(Bottleneck, self).__init__()
+
+        width = int(out_channel * (width_per_group / 64.)) * groups
+
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width,
+                               kernel_size=1, stride=1, bias=False)
+        
+        self.bn1 = nn.BatchNorm2d(width)
+
+        self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups,
+                               kernel_size=3, stride=stride, bias=False, padding=1)
+        
+        self.bn2 = nn.BatchNorm2d(width)
+
+        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel*self.expansion,
+                               kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channel*self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+    
+class ResNet(nn.Module):
+    def __init__(self, 
+                block,
+                blocks_num,
+                num_classes=1000,
+                include_top=True,
+                groups=1,
+                wodth_per_group=64):
+        super(ResNet, self).__init__()
+        self.include_top = include_top
+        self.in_channel = 64
+
+        self.groups = groups
+        self.width_per_group = width_per_group
+
+        
 
