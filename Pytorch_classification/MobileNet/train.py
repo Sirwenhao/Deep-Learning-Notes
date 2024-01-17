c7e084cfa6255fa56802faeaa2d1be0c25774002
Sirwenhao <1547102356@qq.com>
2024-01-17 22:57:47 +0800

    24/01/17

--- Pytorch_classification/MobileNet/train.py
+++ Pytorch_classification/MobileNet/train.py
@@ -31,7 +31,7 @@ def main():
 
 
 
-    data_root =   #daraser path
+    data_root = r'/Users/WH/Desktop/pytorch_classification/flower_data'  #dataset path
     image_path = os.path.join(data_root, "flower_data")
     assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
     train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
@@ -67,7 +67,7 @@ def main():
 
     # load pretrained weights
     # download url: https://download.pytorch.org/models/mobilenet_v2-b0353104.pth
-    model_weight_path = ''
+    model_weight_path = r'/Users/WH/Desktop/pytorch_classification/mobilenet_v2.pth'
     assert os.path.exists(model_weight_path), f"file {model_weight_path} does not exist."
     pre_weights = torch.load(model_v2, map_location='cpu')  # 返回值为一个字典元素，键是参数的名称、值是对应参数的张量
 
@@ -75,4 +75,62 @@ def main():
     # 下述代码使用了字典推导式, numel()是统计张量的元素数量
     pre_dict = {k:v for k,v in pre_weights.items() if net.state_dict()[k].numel() == v.numel()}
     missing_keys, unexpected_keys = net.load_state_dict(pre_dict, strict=False)
+
+    # freeze features weights
+    for param in net.features.parameters():
+        param.requires_grad = False
+
+    net.to(device)
+
+    # define loss function
+    loss_function = nn.CrossEntropyLoss()
+
+    # construct an optimizer
+    params = [p for p in net.parameters() if p.requires_grad]
+    optimizer = optim.Adam(params, lr=0.0001)
+
+    best_acc = 0.0
+    save_path = './MobileNetv2_retrain.pth'
+    train_steps = len(trian_loader)
+    for epoch in range(epochs):
+        # train
+        net.train()
+        running_loss = 0.0
+        train_bar = tqdm(train_loader, file=sys.stdout)
+        for step, data in enumerate(train_bar):
+            images, labels = data
+            optimizer.zero_grad()
+            logits = net(images.to(device))
+            loss = loss_function(logits, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+
+            train_bar.desc = "train epoch[{}/{}] loss:{.3f}".format(epoch+1, epochs, loss)
+
+        # validate
+        net.eval()
+        acc = 0.0 # accumulate accurate number/epoch
+        with torch.no_grad():
+            val_bar = tqdm(validate_loader, file=sys.stdout)
+            for val_data in val_bar:
+                val_images. val_labels = val_data
+                outputs = net(val_images.to(device))
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+                val_bar.desc = 'valid epoch[{}/{}]'.format(epoch+1, epochs)
+
+        val_accurate = acc / val_num
+        print('[epoch %d] train_loss: %.3f val_accuracy: %.3f' %(epoch+1, running_loss/train_steps, val_accurate))
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
+
+    print('Finished Training')
+
+if __name__ == "__main__":
+    main()
 
