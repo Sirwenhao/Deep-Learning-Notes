8a18208dc15ea24a86cfa991760ff7ed2fabf406
Sirwenhao <1547102356@qq.com>
2024-01-18 00:00:11 +0800

    24/01/17

--- Pytorch_classification/MobileNet/train.py
+++ Pytorch_classification/MobileNet/train.py
@@ -25,7 +25,7 @@ def main():
                                     transforms.ToTensor(),
                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
         "val": transforms.Compose([transforms.Resize(256),
-                                   transforms.CenterVrop(224),
+                                   transforms.CenterCrop(224),
                                     transforms.ToTensor(),
                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
 
