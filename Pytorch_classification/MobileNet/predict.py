a9eed3d89463a41479e4900c6a0bc37539e5b2a5
Sirwenhao <1547102356@qq.com>
2024-01-21 11:29:57 +0800

    24/01/21

--- Pytorch_classification/MobileNet/predict.py
+++ Pytorch_classification/MobileNet/predict.py
@@ -23,3 +23,37 @@ def main():
     img = Image.open(img_path)
     plt.imshow(img)
     
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+    
+    # read class_indict
+    json_path = ''
+    assert os.path.exists(json_path), "file: '{}' does not exist.".format(json_path)
+    
+    with open(json_path, "r") as f:
+        class_indict = json.load(f)
+        
+    # create model
+    model = MobileNetV2(num_classes=5).to(device)
+    # load model weights
+    model_weight_path = ""
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+        
+    print_res = "class: {} prob: {:.3f}".format(clas_indict[str(predict_cla)],
+                                                predict[predict-cla].numpy())
+    plt.title(print_res)
+    for i in range(len(predict)):
+        print("class: {:10} prob:{:.3f}".format(class_indict[str(i)],
+                                                predict[i].numpy()))
+    plt.show()
+    
+if __name__ == '__main__':
+    main()
