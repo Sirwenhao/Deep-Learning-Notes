858d4f9d71a3efe9823aabbd97ead5d77d601818
Sirwenhao <1547102356@qq.com>
2024-01-23 22:43:35 +0800

    24/01/23

--- Pytorch_classification/MobileNet/ model_v3.py
+++ Pytorch_classification/MobileNet/ model_v3.py
@@ -31,5 +31,56 @@ class ConvBNActivation(nn.Sequential):
                 groups: int = 1,
                 norm_layer: Optional[Callable[.../, nn.Module]] = None,
                 activation_layer: Optional[Callable[..., nn.Module]] = None):
+        padding = (kernel_size - 1) // 2
+        if norm_layer is None:
+            norm_layer == nn.BatchNorm2d
+        if activation_layer is None:
+            activation_layer = nn.ReLU6
+        super(ConvBNActivation, self).__init__(nn.Conv2d(in_channels=in_planes,
+                                                         out_channels=out_planes,
+                                                         kernel_size=kernel_size,
+                                                         stride=stride,
+                                                         padding=padding,
+                                                         groups=groups,
+                                                         bias=False),
+                                               norm_layer(out_planes),
+                                               activation_layer(inplace=True))
+
+class SqueezeExcitation(nn.Module):
+    def __init__(self, input_c: int, sequeeze_factor: int = 4):
+        super(SqueezeExcitation, self).__init__()
+        squeeze_c = _make_divisible(input_c // squeeze_factor, 8)
+        self.fc1 = nn.Conv2d(input_c, squeeze_c, 1)
+        self.fc2 = nn.Conv2d(squeeze_c, input_c, 1)
+    
+    def forward(self, x:Tensor) -> Tensor:
+        scale = F.adaptive_avg_pool2d(x, output_size=(1, 1))
+        scale = self.fc1(scale)
+        scale = F.relu(scale, inplace=True)
+        scale = self.fc2(scale)
+        scale = F.hardsigmoid(scale, inplace=True)
+        return scale * x
+    
+class InvertedResidualConfig:
+    def __init__(self,
+                 input_c: int,
+                 kernel: int,
+                 expanded_c: int,
+                 out_c: int,
+                 use_se: bool,
+                 activation: str,
+                 stride: int,
+                 width_multi: float):
+        self.input_c = self.adjust_channels(input_c, width_multi)
+        self.kernel = kernel
+        self.expanded_c = self.adjust_channels(expanded_c, width_multi)
+        self.out_c - self.adjust_channels(out_c, width_multi)
+        self.use_se = use_se
+        self.used_hs = activation == "HS"  # whether using h-swish activation
+        self.stride = stride
         
+    @staticmethod # 定义静态方法，静态方法是属于类而不属于实例的方法，可以直接通过类名调用，无需创建类的实例
+    def adjust_channels(channels: int, width_multi: float):
+        return _make_divisibe(channels * width_multi, 8)
+            
     
