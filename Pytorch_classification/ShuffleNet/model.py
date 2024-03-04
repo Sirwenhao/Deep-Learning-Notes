a20b9f4dcfeeeb557973df63ca53a950242f6765
Sirwenhao <1547102356@qq.com>
2024-03-04 22:49:47 +0800

    24/3/4

--- Pytorch_classification/ShuffleNet/model.py
+++ Pytorch_classification/ShuffleNet/model.py
@@ -1,2 +1,24 @@
 # 24/01/28 author:WH
 
+from typing import List, Callable
+
+import torch
+from torch import Tensor
+import torch.nn as nn
+
+def channel_shuffule(x: Tensor, groups: int):
+    batch_size, num_channels, height, width = x.size()
+    channels_per_group = num_channels // groups
+    
+    # reshape
+    # [batch_size, num_channel, height, width] -> [batch_size, groups, channel_per_groups, height, width]
+    x = x.view(batch_size, groups, channels_per_group, height, width)
+    x = torch.transpose(x, 1, 2).contiguous()
+    
+    # flatten 
+    x = x.view(batch_size, -1, height, width)
+    
+    return x
+
+
+    
