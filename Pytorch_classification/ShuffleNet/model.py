5f977d28b6d12206462a589996f1163ece588d45
Sirwenhao <1547102356@qq.com>
2024-03-04 23:00:39 +0800

    24/3/4

--- Pytorch_classification/ShuffleNet/model.py
+++ Pytorch_classification/ShuffleNet/model.py
@@ -21,4 +21,24 @@ def channel_shuffule(x: Tensor, groups: int):
     return x
 
 
-    
+  
+class InvertedResidual(nn.Module):
+    def __init__(self, input_c, output_c, stride):
+        super(InvertedResidual, self).__init__()
+        
+        if stride not in [1, 2]:
+            raise ValueError("illega; stride value.")
+        self.stride = stride
+        
+        assert output_c % 2 == 0
+        branch_features = output_c // 2
+        # stride=1时，input_channel应该是branch_features的两倍
+        # python中'<<'是位运算，可以理解为x2的快速方法
+        assert (self.stride != 1) or (input_c == branch_features << 1)
+        
+        if self.stride == 2:
+            self.branch1 = nn.Sequential(
+                self.depthwise_conv(input_c, input_c, kernel_s=3, stride=self.stride, padding=1),
+                nn.BatchNorm2d(input_c),
+                
+            )
