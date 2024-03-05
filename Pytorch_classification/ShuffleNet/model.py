c4ddcccb17faa5da42aeb0403fa2c5434b9d0a2b
Sirwenhao <1547102356@qq.com>
2024-03-05 22:52:34 +0800

    24/3/5

--- Pytorch_classification/ShuffleNet/model.py
+++ Pytorch_classification/ShuffleNet/model.py
@@ -40,5 +40,160 @@ class InvertedResidual(nn.Module):
             self.branch1 = nn.Sequential(
                 self.depthwise_conv(input_c, input_c, kernel_s=3, stride=self.stride, padding=1),
                 nn.BatchNorm2d(input_c),
-                
+                nn.Conv2d(input_c, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(branch_features),
+                nn.ReLU(inplace=True)
             )
+        else:
+            self.branch1 = nn.Sequential()
+            
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(input_c if self.stride > 1 else branch_features, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(branch_features),
+            self.depthwise_conv(branch_features, branch_features, kernel_s=3, stride=self.stride, padding=1),
+            nn.BatchNorm2d(branch_features),
+            nn.Conv2d(branch_features, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(branch_features),
+            nn.ReLU(inplace=True)
+        )
+    # @staticmethod是python中的一个装饰器，用于定义静态方法。静态方法属于类而非实例，因此定义时不需要self参数，同时使用不需要进行实例化
+    @staticmethod
+    def depthwise_conv(input_c, output_c, kernel_s, stride, padding, bias):
+        return nn.Conv2d(in_channels=input_c, out_channels=output_c, kernel_size=kernel_s, stride=stride, padding=padding, bias=bias, groups=input_c)
+
+    def forward(self, x):
+        if self.stride == 1:
+            # chunk()是PyTorch的一种方法，用于将指定的张量进行切分，2是需要切分的数量，dim=1是切分进行的维度。此例：即在第二维度上将张量x切分成两块
+            x1, x2 = x.chunk(2, dim=1)
+            out = torch.cat((x1, self.branch2(x2)), dim=1)
+        else:
+            out = torch.cat((self.branch1(x), self.branch2(x)), dim=1)
+        out = channel_shuffule(out, 2)
+        return out
+    
+class ShuffleNetV2(nn.Module):
+    def __init__(self, stages_repeats, stages_out_channels, num_classes=1000, inverted_residual=InvertedResidual):
+        super(ShuffleNetV2, self).__init__()
+        
+        if len(stages_repeats) != 3:
+            raise ValueError("expected stages_repeats as list of 3 positive ints")
+        if len(stages_out_channels) != 5:
+            raise ValueError("expected stages_out_channels as list of 5 positive ints")
+        self._stage_out_channels = stages_out_channels
+        
+        # input RGB image
+        input_channels = 3
+        output_channels = self._stage_out_channels[0]
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(inplace=True)  # inplace=True表示直接进行覆盖
+        )
+        input_channels = output_channels
+        
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.stage2: nn.Sequential
+        self.stage3: nn.Sequential
+        self.stage4: nn.Sequential
+        
+        stage_names = ["stage{}".format(i) for i in [2, 3, 4]]
+        for name, repeats, output_channels in zip(stage_names, stages_repeats, self._stage_out_channels[1:]):
+            seq = [inverted_residual(input_channels, output_channels, 2)]
+            for i in range(repeats - 1):
+                seq.append(inverted_residual(output_channels, output_channels, 1))
+                setattr(self, name, nn.Squential(*seq))
+                input_channels = output_channels
+                
+        output_channels = self._stage_out_channels[-1]
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=2, padding=0, bias=False),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.fc = nn.Linear(output_channels, num_classes)
+        
+    def _forward_impl(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = self.conv5(x)
+        x = x.mean([2, 3])
+        x = self.fc(x)
+        return x
+    
+    def forward(self, x):
+        return self._forward_impl(x)
+        
+def shufflenet_v2_x0_5(num_classes=1000):
+    """
+    Constructs a ShuffleNetV2 with 0.5x output channels, as described in
+    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
+    <https://arxiv.org/abs/1807.11164>`.
+    weight: https://download.pytorch.org/models/shufflenetv2_x0.5-f707e7126e.pth
+
+    :param num_classes:
+    :return:
+    """
+    model = ShuffleNetV2(stages_repeats=[4, 8, 4],
+                         stages_out_channels=[24, 48, 96, 192, 1024],
+                         num_classes=num_classes)
+
+    return model
+
+
+def shufflenet_v2_x1_0(num_classes=1000):
+    """
+    Constructs a ShuffleNetV2 with 1.0x output channels, as described in
+    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
+    <https://arxiv.org/abs/1807.11164>`.
+    weight: https://download.pytorch.org/models/shufflenetv2_x1-5666bf0f80.pth
+
+    :param num_classes:
+    :return:
+    """
+    model = ShuffleNetV2(stages_repeats=[4, 8, 4],
+                         stages_out_channels=[24, 116, 232, 464, 1024],
+                         num_classes=num_classes)
+
+    return model
+
+
+def shufflenet_v2_x1_5(num_classes=1000):
+    """
+    Constructs a ShuffleNetV2 with 1.0x output channels, as described in
+    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
+    <https://arxiv.org/abs/1807.11164>`.
+    weight: https://download.pytorch.org/models/shufflenetv2_x1_5-3c479a10.pth
+
+    :param num_classes:
+    :return:
+    """
+    model = ShuffleNetV2(stages_repeats=[4, 8, 4],
+                         stages_out_channels=[24, 176, 352, 704, 1024],
+                         num_classes=num_classes)
+
+    return model
+
+
+def shufflenet_v2_x2_0(num_classes=1000):
+    """
+    Constructs a ShuffleNetV2 with 1.0x output channels, as described in
+    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
+    <https://arxiv.org/abs/1807.11164>`.
+    weight: https://download.pytorch.org/models/shufflenetv2_x2_0-8be3c8ee.pth
+
+    :param num_classes:
+    :return:
+    """
+    model = ShuffleNetV2(stages_repeats=[4, 8, 4],
+                         stages_out_channels=[24, 244, 488, 976, 2048],
+                         num_classes=num_classes)
+
+    return model
+
+
