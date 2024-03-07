9a2a3a3f9542cfc133b161fffff1a933e0151ee6
Sirwenhao <1547102356@qq.com>
2024-03-07 23:01:54 +0800

    24/3/7

--- Pytorch_classification/ShuffleNet/my_dataset.py
+++ Pytorch_classification/ShuffleNet/my_dataset.py
@@ -2,33 +2,36 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 
-class MyDastaSet(Dataset):
+
+class MyDataSet(Dataset):
     """自定义数据集"""
 
-    def __init__(self, images_path, images_class, transform=None):
+    def __init__(self, images_path: list, images_class: list, transform=None):
         self.images_path = images_path
         self.images_class = images_class
         self.transform = transform
-        
+
     def __len__(self):
         return len(self.images_path)
-    
+
     def __getitem__(self, item):
         img = Image.open(self.images_path[item])
-        # RGB为彩色图，L为灰度图
+        # RGB为彩色图片，L为灰度图片
         if img.mode != 'RGB':
-            raise ValueError("image: {} is not RGB mode.".format(self.images_path[item]))
+            raise ValueError("image: {} isn't RGB mode.".format(self.images_path[item]))
         label = self.images_class[item]
-        
+
         if self.transform is not None:
-            img = self.images_class[item]
-            
+            img = self.transform(img)
+
         return img, label
-    
+
     @staticmethod
     def collate_fn(batch):
+        # 官方实现的default_collate可以参考
+        # https://github.com/pytorch/pytorch/blob/67b7e751e6b5931a9f45274653f4f653a4e6cdf6/torch/utils/data/_utils/collate.py
         images, labels = tuple(zip(*batch))
-        
+
         images = torch.stack(images, dim=0)
-        labels = torch.as_tensor(labels)     # torch.as_tensor()创建张量，共享底层数据
+        labels = torch.as_tensor(labels)
         return images, labels
