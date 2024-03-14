753ddd8728cc5679da7b0f99f07a2a804848fd05
Sirwenhao <1547102356@qq.com>
2024-03-14 23:11:09 +0800

    24/3/14

--- Pytorch_classification/DesseNet/model.py
+++ Pytorch_classification/DesseNet/model.py
@@ -73,9 +73,56 @@ class _DenseBlock(nn.ModuleDict):
             features.append(new_features)
         return torch.cat(features, 1)
 class _Transition(nn.Sequential):
-    def __init__(self, input_c,):
-            
+    def __init__(self, input_c, output_c):
+        super(_Transition, self).__init__()
+        self.add_module("norm", nn.BatchNorm2d(input_c))
+        self.add_module("relu", nn.ReLU(inplace=True))
+        self.add_module("conv", nn.Conv2d(input_c, output_c, kernel_size=1, stride=1, bias=False))
+        self.add_module("pool", nn.AdaptiveAvgPool2d(kernel_size=2, stride=2))
         
+class DenseNet(nn.Module):
+    """
+    Densenet-BC model class for imagenet
+
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+          (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient
+    """
     
+    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
+                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000, memory_efficient=False):
+        super(DenseNet, self).__init__()
+        
+        # first conv+bn+relu+pool
+        self.features = nn.Sequential(OrderedDict([
+            ("conv0", nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ("norm0", nn.BatchNorm2d(num_init_features)),
+            ("relu0", nn.ReLU(inplace=True)),
+            ("pool0", nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+            
+        # each dense block
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(num_layers=num_layers, input_c=num_features, bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate, memory_efficient=memory_efficient)
+            self.features.add_module("denseblock %d" %(i+1), block)
+            num_features = num_features + num_layers*growth_rate
+            
+            if i != len(block_config)-1:
+                trans = _Transition(input_c=num_features, output_c=num_features//2)
+                self.features.add_module("transition %d" %(i+1), trans)
+                num_features = num_features // 2
+                
+        # init weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d)
 
 
