048ef0f89178b53cd44ee4d9f05c14e61734ca69
Sirwenhao <1547102356@qq.com>
2024-03-18 23:28:11 +0800

    24/3/18

--- Pytorch_classification/DesseNet/train.py
+++ Pytorch_classification/DesseNet/train.py
@@ -21,7 +21,7 @@ def main(args):
     if os.path.exists("./weights") is False:
         os.makedits("./weights")
         
-    train_images_path, train_images_label, val_images_path, val_images_labels = read_split_data(args.data_path)
+    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(args.data_path)
 
     data_transform = {
         "train": transforms.Compose([transforms.RandomResizedCrop(224),
@@ -33,5 +33,31 @@ def main(args):
                                    transforms.ToTensor(),
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
 
-
+    # 实例化训练数据集
+    train_dataset = MyDataset(images_path=train_images_path,
+                            images_class=train_images_label,
+                            transform=data_transform["train"])
+    
+    # 实例化验证数据集
+    val_dataset = MyDataset(images_path=val_images_path,
+                            images_class=val_images_label,
+                            transform=data_transform["val"])
+    
+    batch_size = args.batch_size
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0,8]) # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+    train_loader = torch.utils.data_DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               num_workers=nw,
+                                               collate_fn=train_dataset.collate_fn)
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=batch_size,
+                                             shuffle=True,
+                                             pin_memory=nw,
+                                             collate_fn=val_dataset.collate_fn)
+    
+    # 载入权重
+    model = densenet121(num_classes=args.num_classes).to(device)
 
