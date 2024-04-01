65fca2f438cc9df14262511ba5c8c1d0a1f26216
Sirwenhao <1547102356@qq.com>
2024-04-01 23:52:42 +0800

    24/4/1

---
+++ Pytorch_classification/EfficientNet/model.py
@@ -0,0 +1,95 @@
+
+import math
+import copy
+from functools import partial
+from collections import OrderedDict
+from typing import Optional, Callable
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.nn import functional as F
+
+def _make_divisible(ch, divisor=8, min_ch=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_ch is None:
+        min_ch = divisor
+    new_ch = max(min_ch, int(ch + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more tha 10%
+    if new_ch < 0.9 * ch:
+        new_ch += divisor
+    return new_ch
+
+def drop_path(x, drop_prob: float= 0., training: bool = False):
+    """
+    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    "Deep Networks with Stochastic Depth", https://arxiv.org/pdf/1603.09382.pdf
+
+    This function is taken from the rwightman.
+    It can be seen here:
+    https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/layers/drop.py#L140
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0], ) + (1, ) * (x.dim - 1) # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_() # binaize
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+class DropPath(nn.Module):
+    """
+    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    "Deep Networks with Stochastic Depth", https://arxiv.org/pdf/1603.09382.pdf
+    """
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+        
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+    
+    
+class ConvBNActivation(nn.Sequential):
+    def __init__(self,
+                 in_planes: int,
+                 out_planes: int,
+                 kernel_size: int = 3,
+                 stride: int = 1,
+                 groups: int = 1,
+                 norm_layer: Optional[Callable[..., nn.Module]] = None,
+                 activation_layer: Optional[Callable[..., nn.Module]] = None):
+        padding = (kernel_size - 1) // 2
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if activation_layer is None:
+            activation_layer = nn.SiLU # alias Swish (torch>=1.7)
+            
+        super(ConvBNActivation, self).__init__(nn.Conv2d(in_channels=in_planes,
+                                                         out_channels=out_planes,
+                                                         kernel_size=kernel_size,
+                                                         stride=stride,
+                                                         padding=padding,
+                                                         groups=groups,
+                                                         bias=False),
+                                               norm_layer(out_planes),
+                                               activation_layer())
+
+class SqueezeExcitation(nn.Module):
+    def __init__(self,
+                 input_c: int,
+                 expand_c: int,
+                 squeeze_factor: int = 4):
+        super(SqueezeExcitation, self).__init__()
+        squeeze_c = input_c // squeeze_factor
+        self.fc1 = nn.Conv2d(expand_c, squeeze_c, 1)
+        self.ac1 = nn.SiLU()
+        self.fc2 = nn.Conv2d(squeeze_c, expand_c, 1)
+        self.ac2 = nn.Sigmoid()
+
