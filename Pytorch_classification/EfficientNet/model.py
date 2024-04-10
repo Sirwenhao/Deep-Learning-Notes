979a25e679b39e7b09d5f56c3cd25162d48fa875
Sirwenhao <1547102356@qq.com>
2024-04-10 21:52:59 +0800

    24/4/10

--- Pytorch_classification/EfficientNet/model.py
+++ Pytorch_classification/EfficientNet/model.py
@@ -202,3 +202,147 @@ class EfficientNet(nn.Module):
                        [5, 80, 112, 6, 1, True, drop_connect_rate, 3],
                        [5, 112, 192, 6, 2, True, drop_connect_rate, 4],
                        [3, 192, 320, 6, 1, True, drop_connect_rate, 1]]
+
+        def round_repeats(repeats):
+            """Round number of repeats based on depth multiplier."""
+            return int(math.ceil(depth_coefficient * repeats))
+        
+        if block is None:
+            block = InvertedResidual
+            
+        if norm_layer is None:
+            norm_layer = partial(nn.BatchNorm2d, eps=1e-3, momentum=0.1)
+
+        adjust_channels = partial(InvertedResidualConfig.adjust_channels, width_coefficient=width_coefficient)
+        
+        # build inverted_rsidual_setting
+        bneck_conf = partial(InvertedResidualConfig, width_coefficient=width_coefficient)
+        
+        b = 0
+        num_blocks = float(sum(round_repeats(i[-1]) for i in default_cnf))
+        inverted_residual_setting = []
+        for stage, args in enumerate(default_cnf):
+            cnf = copy.copy(args)
+            for i in range(round_repeats(cnf.pop(-1))):
+                if i > 0:
+                    # strides equal 1 except first cnf
+                    cnf[-3] = 1 # strides
+                    cnf[1] = cnf[2] # input_channel equal output_channel
+                cnf[-1] = args[-2] * b / num_blocks # update dropout ratio
+                index = str(stage + 1) + chr(i + 97)
+                inverted_residual_setting.append(bneck_conf(*cnf, index))
+        # create layers
+        layers = OrderedDict()
+        
+        # first conv
+        layers.update({"stem_conv": ConvBNActivation(in_planes=3,
+                                                     out_planes=adjust_channels(32),
+                                                     kernel_size=3,
+                                                     stride=2,
+                                                     norm_layer=norm_layer)})        
+        # building inverted residual blocks
+        for cnf in inverted_residual_setting:
+            layers.update({cnf.index: block(cnf, norm_layer)})
+            
+        # build top
+        last_conv_input_c = inverted_residual_setting[-1].out_c
+        last_conv_output_c = adjust_channels(1280)
+        layers.update({"top": ConvBNActivation(in_planes=last_conv_input_c,
+                                               out_planes=last_conv_output_c,
+                                               kernel_size=1,
+                                               norm_layer=norm_layer)})
+        self.features = nn.Sequential(layers)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        
+        classifier = []
+        if dropout_rate > 0:
+            classifier.append(nn.Dropout(p=dropout_rate, inplace=True))
+        classifier.append(nn.Linear(last_conv_output_c, num_classes))
+        self.classifier = nn.Sequential(*classifier)
+        
+        # initial weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, model="fan_out")
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+                elif isinstance(m, nn.BatchNorm2d):
+                    nn.init.ones_(m.weights)
+                    nn.init.zeros_(m.bias)
+                elif isinstance(m, nn.Linear):
+                    nn.init.normal_(m.weight)
+                    nn.init.zeros_(m.bias)
+                    
+    def _forward_impl(self, x: Tensor):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+    
+    def forward(self, x):
+        return self._forward_impl(x)
+    
+    
+def efficientnet_b0(num_classes=1000):
+    # input image size 224x224
+    return EfficientNet(width_coefficient=1.0,
+                        depth_coefficient=1.0,
+                        dropout_rate=0.2,
+                        num_classes=num_classes)
+
+
+def efficientnet_b1(num_classes=1000):
+    # input image size 240x240
+    return EfficientNet(width_coefficient=1.0,
+                        depth_coefficient=1.1,
+                        dropout_rate=0.2,
+                        num_classes=num_classes)
+
+
+def efficientnet_b2(num_classes=1000):
+    # input image size 260x260
+    return EfficientNet(width_coefficient=1.1,
+                        depth_coefficient=1.2,
+                        dropout_rate=0.3,
+                        num_classes=num_classes)
+
+
+def efficientnet_b3(num_classes=1000):
+    # input image size 300x300
+    return EfficientNet(width_coefficient=1.2,
+                        depth_coefficient=1.4,
+                        dropout_rate=0.3,
+                        num_classes=num_classes)
+
+
+def efficientnet_b4(num_classes=1000):
+    # input image size 380x380
+    return EfficientNet(width_coefficient=1.4,
+                        depth_coefficient=1.8,
+                        dropout_rate=0.4,
+                        num_classes=num_classes)
+
+
+def efficientnet_b5(num_classes=1000):
+    # input image size 456x456
+    return EfficientNet(width_coefficient=1.6,
+                        depth_coefficient=2.2,
+                        dropout_rate=0.4,
+                        num_classes=num_classes)
+
+
+def efficientnet_b6(num_classes=1000):
+    # input image size 528x528
+    return EfficientNet(width_coefficient=1.8,
+                        depth_coefficient=2.6,
+                        dropout_rate=0.5,
+                        num_classes=num_classes)
+
+
+def efficientnet_b7(num_classes=1000):
+    # input image size 600x600
+    return EfficientNet(width_coefficient=2.0,
+                        depth_coefficient=3.1,
+                        dropout_rate=0.5,
+                        num_classes=num_classes)    
