8d4187535655589f30f4774ee45a4f1b13e3e497
Sirwenhao <1547102356@qq.com>
2024-05-10 23:19:06 +0800

    24/5/10

--- Pytorch_classification/ConfusionMatrix/model.py
+++ Pytorch_classification/ConfusionMatrix/model.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from prettytable import PrettyTable
 from tqdm import tqdm
 from torchvision import transforms, datasets
-# from model import MobileNetV2
+from model import MobileNetV2
 
 
 class ConfusionMatrix(object):
@@ -35,8 +35,78 @@ class ConfusionMatrix(object):
             FP = np.sum(self.matrix[i, :]) - TP
             FN = np.sum(self.matrix[:, i]) - TP
             TN = np.sum(self.matrix) - TP - FP - FN
+            Precision = round(TP / (TP+FP), 3) if TP+FP !=0 else 0.
+            Recall = round(TP / (TP+FN), 3) if TP+FN != 0 else 0.
+            Specificity = round(TN / (TN+FP), 3) if TN+FP != 0 else 0.
+            table.add_row([self.labels[i], Precision, Recall, Specificity])
+        print(table) 
             
-
+    def plot(self):
+        matrix = self.matrix
+        print(matrix)
+        plt.imshow(matrix, cmap=plt.cn.Blues)
+        
+        plt.xticks(range(self.num_classes), self.labels, rotation=45)
+        plt.yticks(range(self.num_classes), self.labels)
+        plt.colorbar()
+        plt.xlabel('True Lables')
+        plt.ylabel('Predicted Labels')
+        plt.title('Confusion matrix')
+        
+        thresh = matrix.max() / 2
+        for x in range(self.num_classes):
+            for y in range(self.num_classes):
+                info = int(matrix[x, y])
+                plt.text(x, y, info, verticalalignment='center',
+                         horizontalalignment='center',
+                         color="white" if info > thresh else "black")
+        plt.tight_layout()
+        plt.show()
+        
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+    
+    data_transform = transforms.Compose([transforms.Resize(256),
+                                         transforms.CenterCrop(224),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+    image_path = os.path.join(data_root, "data_set", "flower_data")
+    assert os.path.exists(image_path), "data path {} does not exist.".format(image_path)
+    
+    validate_dateset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                            transform=data_transform)
+    batch_size = 16
+    validate_loader = torch.utils.data.DataLoader(validate_dateset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    net = MobileNetV2(num_classes=5)
+    # load model weigths
+    model_weight_path = "./MobileNetV2.pth"
+    assert os.path.exists(model_weight_path), "cannot find {} file".format(image_path)
+    net.load_state_dict(torch.load(model_weight_path, map_location=device))
+    net.to(device)
+    
+    # read class_indict
+    json_label_path = './class_indices.json'
+    assert os.path.exists(json_label_path), "cannot find {} file".format(json_label_path)
+    json_file = open(json_label_path, 'r')
+    class_indict = json.load(json_file)
+    
+    labels = [label for _, label in class_indict.items()]
+    confusion = ConfusionMatrix(num_classes=5, labels=labels)
+    net.eval()
+    with torch.no_grad():
+        for val_data in tqdm(validate_loader):
+            val_imags, val_labels = val_data
+            outputs = net(val_imags).to(device)
+            outputs = torch.softmax(outputs, dim=1)
+            outputs = torch.argmax(outputs, dim=1)
+            confusion.update(outputs.to("cpu").numpy(), val_labels.to("cpu").numpy())
+    confusion.plot()
+    confusion.summary()
+            
+    
 
 
 
