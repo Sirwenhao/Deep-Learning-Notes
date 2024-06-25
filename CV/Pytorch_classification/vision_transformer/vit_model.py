5c91ffa094b7dc56f8502fd2589dea53f4ff5be0
Sirwenhao <1547102356@qq.com>
2024-06-25 22:39:59 +0800

    24/6/25

--- CV/Pytorch_classification/vision_transformer/vit_model.py
+++ CV/Pytorch_classification/vision_transformer/vit_model.py
@@ -117,12 +117,12 @@ class Block(nn.Module):
         return x 
     
     
-class VisonTransformer(nn.Module):
+class VisionTransformer(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_c=3, num_classes=1000,
                  embed_dim=768, depth=12, num_heads=12, mlp_ratio=4.0, qkv_bias=True,
                  qk_scale=None, representation_size=None, distilled=False, drop_ratio=0.,
                  attn_drop_ratio=0., drop_path_ratio=0., embed_layer=PatchEmbed, norm_layer=None):
-        super(VisonTransformer, self).__init__()
+        super(VisionTransformer, self).__init__()
         self.num_classes = num_classes
         self.num_features = self.embed_dim = embed_dim
         self.num_tokens = 2 if distilled else 1
@@ -142,3 +142,166 @@ class VisonTransformer(nn.Module):
                   drop_ratio=drop_ratio, attn_drop_ratio=attn_drop_ratio, drop_path_ratio=dpr[i], num_layer=norm_layer, act_layer=act_layer) for i in range(depth)
         ])
         self.norm = norm_layer(embed_dim) 
+
+        # Representation layer
+        if representation_size and not distilled:
+            self.has_logits = True
+            self.num_features = representation_size
+            self.pre_logits = nn.Sequential(OrderedDict([
+                ("fc", nn.Linear(embed_dim, representation_size)),
+                ("act", nn.Linear())
+            ]))
+        else:
+            self.has_logits = False
+            self.pre_logits = nn.Identity()
+            
+        # classifier head(s)
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+        self.head_dist = None
+        if distilled:
+            self.head_dist = nn.Linear(self.embed_dim, self.num_classes) if num_classes > 0 else nn.Identity()
+            
+        # weight init
+        nn.init.trunc_normal_(self.pos_embed, std=0.02)
+        if self.disk_token is not None:
+            nn.init.trunc_normal_(self.disk_token, std=0.02)
+            
+        nn.init.trunc_normal_(self.cls_token, std=0.02)
+        self.apply(_init_vit_weights)
+        
+    def forward_features(self, x):
+        x = self.patch_embed(x)
+        cls_token = self.cls_token.expand(x.shape[0], -1, -1)
+        if self.disk_token is None:
+            x = torch.cat((cls_token, x), dim=1)
+        else:
+            x = torch.cat((clas_token, self.disk_token.expand(x.shape[0], -1, -1)), dim=1)
+            
+        x = self.pos_drop(x + self.pos_embed)
+        x = self.blocks(x)
+        x = self.norm(x)
+        if self.disk_token is None:
+            return self.pre_logits(x[:, 0])
+        else:
+            return x[:, 0], x[:, 1]
+        
+    def forward(self, x):
+        x = self.forward_features(x)
+        if self.head_dist is not None:
+            x, x_dist = self.head(x[0]), self.head_dist(x[1])
+            if self.training and not torch.jit.is_scripting():
+                return x, x_dist
+            else:
+                x = self.head(x)
+        return x
+    
+    def _init_vit_weights(m):
+        if isinstance(m, nn.Linear):
+            nn.init.trunc_normal_(m.weight, std=.01)
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.Conv2d):
+            nn.init.kaiming_normal_(m.weight, mode="fan_out")
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.zeros_(m.bias)
+            nn.init.ones_(m.weight)
+            
+            
+    def vit_base_patch16_224(num_classes = 1000):
+        model = VisionTransformer(img_size=224,
+                                  patch_size=16,
+                                  embed_dim=768,
+                                  depth=12,
+                                  num_heads=12,
+                                  representation_size=None,
+                                  num_classes=num_classes)
+        return model
+    
+    def vit_base_patch16_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+
+        model = VisionTransformer(img_size=224,
+                                patch_size=16,
+                                embed_dim=768,
+                                depth=12,
+                                num_heads=12,
+                                representation_size=768 if has_logits else None,
+                                num_classes=num_classes)
+        return model
+
+
+    def vit_base_patch32_224(num_classes: int = 1000):
+
+        model = VisionTransformer(img_size=224,
+                                patch_size=32,
+                                embed_dim=768,
+                                depth=12,
+                                num_heads=12,
+                                representation_size=None,
+                                num_classes=num_classes)
+        return model
+
+
+    def vit_base_patch32_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+
+        model = VisionTransformer(img_size=224,
+                                patch_size=32,
+                                embed_dim=768,
+                                depth=12,
+                                num_heads=12,
+                                representation_size=768 if has_logits else None,
+                                num_classes=num_classes)
+        return model
+
+
+    def vit_large_patch16_224(num_classes: int = 1000):
+
+        model = VisionTransformer(img_size=224,
+                                patch_size=16,
+                                embed_dim=1024,
+                                depth=24,
+                                num_heads=16,
+                                representation_size=None,
+                                num_classes=num_classes)
+        return model
+
+
+    def vit_large_patch16_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+ 
+        model = VisionTransformer(img_size=224,
+                                patch_size=16,
+                                embed_dim=1024,
+                                depth=24,
+                                num_heads=16,
+                                representation_size=1024 if has_logits else None,
+                                num_classes=num_classes)
+        return model
+
+
+    def vit_large_patch32_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+
+        model = VisionTransformer(img_size=224,
+                                patch_size=32,
+                                embed_dim=1024,
+                                depth=24,
+                                num_heads=16,
+                                representation_size=1024 if has_logits else None,
+                                num_classes=num_classes)
+        return model
+
+
+    def vit_huge_patch14_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+ 
+        model = VisionTransformer(img_size=224,
+                                patch_size=14,
+                                embed_dim=1280,
+                                depth=32,
+                                num_heads=16,
+                                representation_size=1280 if has_logits else None,
+                                num_classes=num_classes)
+        return model
+                
+            
+            
+                
