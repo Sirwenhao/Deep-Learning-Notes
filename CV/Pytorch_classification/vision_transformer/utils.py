8446718d9c1dc9170dd87e417a83208cfbce2492
Sirwenhao <1547102356@qq.com>
2024-06-27 22:58:42 +0800

    24/6/27

--- CV/Pytorch_classification/vision_transformer/utils.py
+++ CV/Pytorch_classification/vision_transformer/utils.py
@@ -90,4 +90,71 @@ def plot_data_loader_image(data_loader):
 def write_pickle(list_info, file_name):
     with open(file_name, 'wb') as f:
         pickle.dump(list_info, f)
+        
+def rad_pickle(file_name):
+    with open(file_name, 'rb') as f:
+        info_list = pickle.load(f)
+        return info_list
+    
+def train_one_epoch(model, optimizer, data_loader, device, epoch):
+    model.train()
+    loss_function = torch.nn.CrossEntropyLoss()
+    accu_loss = torch.zeros(1).to(device)
+    accu_num = torch.zeros(1).to(device)
+    optimizer.zero_grad()
+    
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+        
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred, labels.to(device)).sum()
+        
+        loss = loss_function(pred, labels.to(device))
+        loss.backward()
+        accu_loss += loss.detach()
+        
+        data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch, accu_loss.item() / (step + 1), accu_num.item() / sample_num)
+        
+        if not torch.isfinite(loss):
+            print("Warning: non-finiti loss, ending training", loss)
+            sys.exit(1)
+            
+        optimizer.step()
+        optimizer.zero_grad()
+        
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+@ torch.no_grad()
+def evaluate(model, data_loader, device, epoch):
+    loss_function = torch.nn.CrossEntropyLoss()
+    
+    model.eval()
+    
+    accu_num = torch.zeros(1).to(device)
+    accu_loss = torch.zeros(1).to(device)
+    
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+        
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+        
+        loss = loss_function(pred, labels.to(device))
+        accu_loss += loss
+        
+        data_loader.desc = "[Valid epoch {}] loss: {:.3f}, acc: {:,3f}".format(epoch, accu_loss.item()/(step+1), accu_num()/sample_num)
+        
+    return accu_loss.item()/(step+1), accu_num.item()/sample_num
+            
+        
+        
+    
     
