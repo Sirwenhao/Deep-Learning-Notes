1eb2202167a85790cc86530fb43d06848bf60128
Sirwenhao <1547102356@qq.com>
2024-07-02 23:12:49 +0800

    24/7/2

---
+++ CV/Pytorch_classification/grad_cam/utils.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+class ActivationAndGradients:
+    def __init__(self, model, target_layers, reshape_transform):
+        self.model = model
+        self.gradients = []
+        self.activations = []
+        self.reshape_transform = reshape_transform
+        self.handles = []
+        for target_layer in target_layers:
+            self.handles.append(
+                target_layer.append(
+                    target_layer.register_forward_hook(
+                        self.save_activastion))
+            if hasattr(target_layer, 'register_full_backward_hook'):
+                self.handles.append(
+                    target_layer.register_full_backward_hook(
+                        self.save_gradient))
+            else:
+                self.handles.append(
+                    target_layer.register_backwarrd_hook(
+                        self.save_gradients))
+    
+
