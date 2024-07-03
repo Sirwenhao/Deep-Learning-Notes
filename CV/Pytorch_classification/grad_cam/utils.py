d984fe9feb42126aff266b6996a93d6f255dd82a
Sirwenhao <1547102356@qq.com>
2024-07-03 23:42:08 +0800

    24/7/3

--- CV/Pytorch_classification/grad_cam/utils.py
+++ CV/Pytorch_classification/grad_cam/utils.py
@@ -22,4 +22,84 @@ class ActivationAndGradients:
                     target_layer.register_backwarrd_hook(
                         self.save_gradients))
     
+    def save_activation(self, module, input, output):
+        activation = output
+        if self.reshape_transform is not None:
+            activation = self.reshape_transform(activation)
+        self.activations.append(activation.cpu().detach())
+        
+    def save_gradient(self, module, grad_input, grad_output):
+        grad = grad_output[0]
+        if self.reshape_transform is not None:
+            grad = self.reshape_transform(grad)
+        self.gradients += [grad.cpu().detach()]
+        
+    def __call__(self, x):
+        self.gradients = []
+        self.activations = []
+        return self.model(x)
+        
+    def release(self):
+        for handle in self.handles:
+            handle.remove()
+            
+class GradCAM:
+    def __init__(self, model, target_layers, reshape_transform=None, use_cuda=False):
+        self.model = model.eval()
+        self.target_layers = target_layers
+        self.reshape_transform = reshape_transform
+        self.cuda = use_cuda
+        if self.cuda:
+            self.model = model.cuda()
+        self.activations_and_grads = ActivationAndGradients(
+            self.model, target_layers, reshape_transform)
+        
+    @staticmethod
+    def get_cam_weights(grads):
+        return np.mean(grads, axis=(2, 3), keepdim=True)
+        
+    @staticmethod
+    def get_loss(output, target_category):
+        loss = 0
+        for i in range(len(target_category)):
+            loss = loss + output[i, target_category[i]]
+        return loss
+        
+    def get_cam_image(self, activations, grads):
+        weights = self.get_cam_weights(grads)
+        weighted_activations = weights * activations
+        cam = weighted_activations.sum(axis=1)
+        return cam
+        
+    @staticmethod
+    def get_target_width_height(input_tensor):
+        width, height = input_tensor.size(-1), input_tensor.size(-2)
+        return width, height
+        
+    def compute_cam_per_layer(self, input_tensor):
+        activations_list = [a.cpu().data.numpy()
+                            for a in self.activations_and_grads.activations]
+        grads_list = [g.cpu().data.numpy()
+                      for g in slef.activations_and_grads_gradients]
+        target_size = self.get_target_width_height(input_tensor)
+        
+        cam_per_target_layer = []
+        
+        # Loop over the saliency image from every layer
 
+        for layer_activations, layer_grads in zip(activations_list, grads_list):
+            cam = self.get_cam_image(layer_activations, layer_grads)
+            cam[cam < 0] = 0  # works like mute the min-max scale in the function of scale_cam_image
+            scaled = self.scale_cam_image(cam, target_size)
+            cam_per_target_layer.append(scaled[:, None, :])
+
+        return cam_per_target_layer
+
+        
+    def aggregate_multi_layers(self, cam_per_target_layer):
+        cam_per_target_layer = np.concatenate(cam_per_target_layer, axis=1)
+        cam_per_target_layer = np.maxmium(cam_per_target_layer, 0)
+        result = np.mean(cam_per_target_layer, axis=1)
+        return self.scale_cam_image(result)
+    
+        
