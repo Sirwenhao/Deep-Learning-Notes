0019395659c92ced599e7ac34f2172dbb03aacd3
Sirwenhao <1547102356@qq.com>
2024-07-15 22:53:20 +0800

    24/7/15

--- CV/Pytorch_classification/grad_cam/utils.py
+++ CV/Pytorch_classification/grad_cam/utils.py
@@ -102,4 +102,91 @@ class GradCAM:
         result = np.mean(cam_per_target_layer, axis=1)
         return self.scale_cam_image(result)
     
+    
+    @staticmethod
+    def scale_cam_image(cam, target_size=None):
+        result = []
+        for img in cam:
+            img = img - np.min(img)
+            img = img / (1e-7 + np.max(img))
+            if target_size is not None:
+                img = cv2.resize(img, target_size)
+            result.append(img)
+        result = np.float32(result)
+        return result
+    
+    def __call__(self, input_tensor, target_category=None):
+        if self.cuda:
+            input_tensor = input_tensor.cuda()
+            
+        # 正向传播得到网络输出logits
+        output = self.activations_and_grads(input_tensor)
+        if isinstance(target_category, int):
+            target_category = [target_category] * input_tensor.size(0)
+            
+        if target_category is None:
+            target_category = np.argmax(output.cpu().data.numpy(), axis=-1)
+            print(f'category id:{target_category}')
+        else:
+            assert (len(target_category) == input_tensor.size(0))
+            
+        self.model.zero_grad()
+        loss = self.get_loss(output, target_category)
+        loss.backward(retrain_graph=True)
         
+        cam_per_layer = self.compute_cam_per_layer(input_tensor)
+        return self.aggregate_multi_layers(cam_per_layer)
+        
+    def __del__(self):
+        self.activations_and_grads.release()
+        
+    def __enter__():
+        return self
+        
+    def __exit__(self):
+        self.activations_and_grads.release()
+        if isinstance(exc_value, IndexError):
+            print(f'An exception occurred in CAM with block: {exc_type}. Message: {exc_value}')
+        return True
+        
+    def show_cam_on_image(img, mask, use_rgb, colormap=cv2.COLORMAP_JET):
+        heatmap = cv2.applyColorMap(np.uint8(255 * mask), colormap)
+        if use_rgb:
+            heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
+        heatmap = np.float32(heatmap) / 255
+        
+        if np.max(img) > 1:
+            raise Exception("The input image should np.float32 in the range [0, 1]")
+            
+        cam = heatmap + img
+        cam = cam / np.max(cam)
+        return np.uint8(255 * cam)
+        
+    def center_crop_img(img, size):
+        h, w, c = img.shape
+        
+        if w == h == size:
+            return img
+            
+        if  w < h:
+            ratio = size / w
+            new_w = size
+            new_h = int(h * ratio)
+        else:
+            ratio = size / h
+            new_h = size
+            new_w = int(w * ratio)
+            
+        img = cv2.resize(img, dsize=(new_w, new_h))
+        
+        if new_w == size:
+            h = (new_h - size) // 2
+            img = img[h: h+size]
+        else:
+            w = (new - size) // 2
+            img = img[:, w:w+size]
+        return img
+        
+        
+            
+              
