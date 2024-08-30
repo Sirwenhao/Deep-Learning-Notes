e6eb205d3d40b08dcbdafe07ee47413a1c3171c4
Sirwenhao <1547102356@qq.com>
2024-08-31 00:26:26 +0800

    24/8/30

--- CV/Pytorch_classification/CRNN_PyTorch/train.py
+++ CV/Pytorch_classification/CRNN_PyTorch/train.py
@@ -25,12 +25,64 @@ parser.add_argument('--workers', type=int, help='number of data loading workers'
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imgH', type=int, default=32, help='the height of the input image to network')
 parser.add_argument('--imgW', type=int, default=100, help='the width of the input image to network')
-parser.add_argument()
-parser.add_argument()
-parser.add_argument()
-parser.add_argument()
-parser.add_argument()
-parser.add_argument()
+parser.add_argument('--nh', type=int, default=256, help='size of the input image to network')
+parser.add_argument('--nepoch', type=int, default=25, help='number of epoches to train for')
+parser.add_argument('--cuda', action='store_true', helps='enables cuda')
+parser.add_argument('--ngpu', tyype=int, default=1, help='number of GPUs to use')
+parser.add_argument('--pretrained', default=1, help='path to pretrained model (to continue training)')
+parser.add_argument('--alphabet', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz')
+parser.add_argument('--exp_dir', default='expr', help='Where to store samples and models')
+parser.add_argument('--displayInterval', type=int, default=500, help='Interval to be displayed')
+parser.add_argument('--n_test_disp', type=int, default=10, help='Number of samples to display when test')
+parser.add_argument('--valInterval', type='int', default=500, help='Interval to be displayed')
+parser.add_argument('--saveInterval', type=int, default=500, help='Interval to be displayed')
+parser.add_argument('--lr', type=float, default=0.01, help='learning rate for Critic, not used by adadealta')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta11 for adam. default=0.5')
+parser.add_argument('--adam', action='store_true', help='whether to use adam (default is rmsprop)')
+parser.add_argument('--adadelta', action='store_true', help='Whether to use adadelta (default is rmsprop)')
+parser.add_argument('keep_ratio', action='store_true', help='whether to keep ratio for image resize')
+parser.add_argument('--manualSeed', type=int, default=1234, help='reproduce experiment')
+parser.add_argument('--random_sample', action='store_true', help='whether to sample the dataset with random sampler')
+opt = parser.parse_agrs()
+print(opt)
+
+if not os.path.exists(opt.expr_dir):
+    os.makedirs(opt.expr_dir)
+    
+random.seed(opt.manualSeed)
+np.random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+cudnn.benchmark = True
+
+if torch.cuda.is_available() and not opt.cuda:
+    print(f'WARNING: Youhave a CUDA device, so you should probably run with --cuda')
+    
+train_dataset = dataset.lmdbDastaset(root=opt.trainroot)
+assert train_dataset
+if not opt.radnom_sample:
+    sampler = dataset.randomSequentialSampler(train_dataset, opt.batchSize)
+else:
+    sampler = None
+    
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=opt.batchSize,
+    shuffle=True, sampler=sampler,
+    num_workers=int(opt.workers),
+    collate_fn=dataset.alignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio=opt.keep_atio))
+
+test_dataset = dataset.lmdbDastaset(
+    root=opt.valroot, transform=dataset.resizedNormalize((100, 32)))
+
+nclass = len(opt.alphabet) + 1
+nc = 1
+
+
+
+        
+        
+        
+
 
 
 
